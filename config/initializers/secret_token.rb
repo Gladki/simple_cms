32f3fe13aea0c581787e8451fce528d8c1581ab4
Gladki <alexanderglad@gmail.com>
2013-02-06 22:01:46 +0100

    s

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '914995c9027538794f347649aa05534b564e2c44a02f181570e0c8edf3b6b571bad8e1040b796cc8d2b488bff2cc52bf41d1911f04689e1072095ae784e977cb'
---
+++ db/migrate/20130202224455_do_nothing_yet.rb
@@ -0,0 +1,9 @@
+class DoNothingYet < ActiveRecord::Migration
+  def up
+    #instructions for changing db to a new state
+  end
+
+  def down
+    #changing db to previous state
+  end
+end
