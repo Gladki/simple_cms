d976ccd64f186aab8b3e612d32f38f83c4bfd3f9
Gladki <alexanderglad@gmail.com>
2013-02-27 21:28:10 +0100

    migracja effektywność

--- db/migrate/20130223222514_create_effectivities.rb
+++ db/migrate/20130223222514_create_effectivities.rb
@@ -2,7 +2,7 @@ class CreateEffectivities < ActiveRecord::Migration
   def change
     create_table :effectivenes do |t|
       t.integer :id
-      t.integer :id_worker
+      t.integer :worker_id
       t.decimal :effectivity
       t.decimal :effectivity_min
       t.decimal :effectivity_max
