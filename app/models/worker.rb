d45623f596bf126728c0e07ea271273723bf2fc3
Gladki <alexanderglad@gmail.com>
2013-02-28 03:22:50 +0100

    dodanie modułu wyszukiwnaiav2

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -3,13 +3,13 @@ class Worker < ActiveRecord::Base
 
   has_many :effectivenes, :class_name => "Effectivenes"
 
-		def self.search(search)
+	def self.search(search)
 		if search
-		find(:all, :conditions => ['workers.id LIKE ?', "%#{search}%"])
+			find(:all, :conditions => ['workers.id LIKE (?)', "%#{search}%"])
 		else
-		find(:all)
+			find(:all, :conditions => ['effectivenes.created_at = '''])
 		end
-		end
+	end
 
 
 end
